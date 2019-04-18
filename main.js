254a1b05137ca13039e27b4d0ac4232160c7d8e9
Olusoladeboy <eoyewole508@stu.ui.edu.ng>
2019-04-18 10:51:06 +0100

    Added Sign Up Page

--- main.js
+++ main.js
@@ -11,14 +11,35 @@ $(document).ready(function(){
   });
 
   var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:4,
+
+
+  $('.owl-carousel').owlCarousel({
+    stagePadding: 50,
     loop:true,
-    margin:1,
+    margin:10,
+    nav:true,
     autoplay:true,
-    autoWidth: true,
-    autoHeight: true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:false
-});
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+// owl.owlCarousel({
+//     stagePadding: 1000,
+//     items:4,
+//     loop:true,
+//     margin:10,
+//     autoplay:true,
+//     autoWidth: true,
+//     autoHeight: true,
+//     autoplayTimeout:3000,
+//     autoplayHoverPause:false
+// });
 
